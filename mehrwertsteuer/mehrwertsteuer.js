4324d3e168eb2072ba19c47c7c50e67432ad580c
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-08-25 15:45:32 +0200

    Fehler behoben

--- mehrwertsteuer/mehrwertsteuer.js
+++ mehrwertsteuer/mehrwertsteuer.js
@@ -1,5 +1,5 @@
 // Berechnet die Mehrwertsteuer für einen Betrag (z. B. 19%).
-function mwst(betrag, satz = 0.2) {
+function mwst(betrag, satz = 0.19) {
   const steuer = betrag * satz;
   // auf 2 Nachkommastellen runden (Geldbeträge)
   return Math.round(steuer * 100) / 100;
